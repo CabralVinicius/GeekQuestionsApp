2f6b4a84956be1f0e22b3ee4eb7d270a30329b6a
ViniciusCabral <cabral.vm@outlook.com>
2023-05-01 18:29:07 -0300

    Consegui terminar a logica para fazer os elementos da view se alterarem

--- Prototipo1/QuestionsView.swift
+++ Prototipo1/QuestionsView.swift
@@ -27,30 +27,44 @@ extension View {
 }
 
 struct QuestionsView: View {
+    @State private var currentQuestionIndex = 0
+    @State private var score = 0
+    
+    private var currentQuestion: Questions {
+        questions[currentQuestionIndex]
+    }
+    
+    private func checkAnswer(_ answerIndex: Int) {
+        let answer = currentQuestion.alternatives[answerIndex]
+        if answer.correct {
+            score += 1
+        }
+        
+        if currentQuestionIndex < questions.count - 1 {
+            currentQuestionIndex += 1
+        }
+    }
+    
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [Color("color1"), Color("color2")]), startPoint: .top, endPoint: .bottom).ignoresSafeArea()
             
             VStack(spacing: 30) {
-                Text(questions[0].question)
+                Text(currentQuestion.question)
                     .multilineTextAlignment(.center)
                     .frame(height: 110)
                     .foregroundColor(.white)
                     .font(.system(size: 30))
                     .padding()
                 
-                Button("a) Tolkien"){}
+                ForEach(currentQuestion.alternatives.indices) { index in
+                    Button("\(index+1)) \(currentQuestion.alternatives[index].text)") {
+                        checkAnswer(index)
+                    }
                     .buttonStyleQuestions()
-                Button("b) Boninho"){}
-                    .buttonStyleQuestions()
-                Button("c) Emílio Surita"){}
-                    .buttonStyleQuestions()
-                Button("d) Clarice Lispector"){}
-                    .buttonStyleQuestions()
-                Button("e) BRKs Edu"){}
-                    .buttonStyleQuestions()
+                }
                 
-                Text("Score: 0")
+                Text("Score: \(score)")
                     .foregroundColor(Color("color1"))
                     .font(.system(size: 30))
                     .padding()
